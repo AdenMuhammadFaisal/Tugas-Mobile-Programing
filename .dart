704e7d502ad22f47a60211a43fbba116bbff39c6
AdenMuhammadFaisal <127273098+AdenMuhammadFaisal@users.noreply.github.com>
2023-03-09 23:28:33 +0700

    Create .dart

---
+++ .dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+Run | Debug| Profile 
+void main() 
+  { runApp(const MyApp());
+
+You, 36 seconds ago | 1 author (You)
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+// This widget is the root of your application.
+@override
+Widget build(BuildContext context) {
+  return MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Aden M Faisal'),   You, 1 second
+      ), // AppBar
+      body: Center(
+        child: Text("Hello Word"),
+      ), // Center 
+    ), // Scaffold
+  ); // MaterialApp
+ }
+}
